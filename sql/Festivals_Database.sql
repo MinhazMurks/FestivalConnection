092e3988d91a66c1f2ede52e26aa9a31542d3931
MinhazMurks <minhazabdullah1@gmail.com>
2018-04-18 21:24:04 -0400

    Added sql script for database (WIP)

---
+++ sql/Festivals_Database.sql
@@ -0,0 +1,72 @@
+create database if not exists festivals_project;
+use festivals_project;
+
+create table Festival(
+ festID Decimal(9,0) primary key,
+ fullname varchar(50),
+ location varchar(50),
+ production_comp bool,
+ start_date date,
+ end_date date,
+ price decimal(9,2)
+);
+
+create table Users(
+ userID         Decimal(9,0) primary key,
+ fullname       varchar(50),
+ birthdate      date,
+ user_location  varchar(50) references Location(city, state, streetAddress),
+ is_company     bool
+);
+
+create table Friends(
+  user1         Decimal(9,0) references Users(userID),
+  user2         Decimal(9,0) references Users(userID),
+  primary key (user1, user2)
+);
+
+create table Bookmarks(
+  userID       Decimal(9,0) references Users(userID),
+  festID       Decimal(9,0) references Festival(festID),
+  primary key (userID, festID)
+);
+
+create table Location(
+ city           varchar(20),
+ state          varchar(20),
+ streetAddress  varchar(50),
+ address        varchar(20),
+ primary key (city, state, streetaddress)
+);
+
+create table Providers(
+  festID       Decimal(9, 0) references Festival(festID),
+  name         varchar(50),
+  primary key (festID, name)
+);
+
+create table Music(
+  festID       Decimal(9,0) references Festival(festID),
+  musicians    varchar(20)  references Providers(festID, name),
+  genre        varchar(20),
+  outdoor      bool,
+  camping      bool
+);
+
+create table Comedy(
+  festID       Decimal(9,0) references Festival(festID),
+  comedians    varchar(50)  references Providers(festID, name)
+);
+
+create table Art(
+  festID       Decimal(9,0) references Festival(festID),
+  artist       varchar(20) references Providers(festID, name),
+  genre        varchar(20)
+);
+
+create table Beer(
+  festID       Decimal(9,0) references Festival(festID),
+  breweries    varchar(50) references Providers(festID, name)
+);
+
+
