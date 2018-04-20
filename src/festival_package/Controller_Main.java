1e58c8cc570c28389119033e9e25b1c35e83f3a6
MinhazMurks <minhazabdullah1@gmail.com>
2018-04-20 02:07:52 -0400

    UI Coming along 2

--- src/festival_package/Controller_Main.java
+++ src/festival_package/Controller_Main.java
@@ -2,13 +2,33 @@ package festival_package;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
 public class Controller_Main {
 
 
-    @FXML
+
     public void on_search_button(ActionEvent event)
     {
         System.out.println("Fuuuuck!");
     }
+
+
+    public void on_friend_button(ActionEvent event)
+    {
+        System.out.println("Friend adding");
+    }
+
+
+    public void on_favorite_button(ActionEvent event)
+    {
+        System.out.println("Favorite adding");
+    }
+
+    public void on_advanced_button(ActionEvent event)
+    {
+        System.out.println("advanced search");
+    }
+
 }
--- src/festival_package/Main.java
+++ src/festival_package/Main.java
@@ -20,6 +20,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main_window.fxml"));
 
+        root.getStylesheets().add("festival_package/Styles.css");
+
         primaryStage.setScene(new Scene(root,1280, 800));
         primaryStage.getIcons().add(new Image("file:src/festival_package/resources/Festival Logo2.png"));
         primaryStage.setTitle("Festival Connection");
