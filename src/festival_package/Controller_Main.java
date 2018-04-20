f78b29f4bec2657b297ab312005d52eca4404c17
Hughbert001 <longhr@vcu.edu>
2018-04-20 15:46:10 -0400

    GUI UPDATE interweaving scenes

--- src/festival_package/Controller_Main.java
+++ src/festival_package/Controller_Main.java
@@ -1,11 +1,36 @@
 package festival_package;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 
 public class Controller_Main {
+
+    private Stage primaryStage = new Stage();
+    private AnchorPane rootPane;
+
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(
+                getClass().getResource(fxml));
+        //#1
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image("file:src/festival_package/resources/Festival Logo2.png"));
+        primaryStage.setTitle(fxml);
+        primaryStage.show();
+
+    }
 
 
 
@@ -15,19 +40,21 @@ public class Controller_Main {
     }
 
 
-    public void on_friend_button(ActionEvent event)
+    public void on_friend_button(ActionEvent event) throws IOException
     {
+        changeScene("Bookmarks.fxml");
         System.out.println("Friend adding");
     }
 
 
-    public void on_favorite_button(ActionEvent event)
+    public void on_favorite_button(ActionEvent event) throws IOException
     {
+        changeScene("Bookmarks.fxml");
         System.out.println("Favorite adding");
     }
 
-    public void on_advanced_button(ActionEvent event)
-    {
+    public void on_advanced_button(ActionEvent event) throws IOException {
+        changeScene("AdvancedSearch.fxml");
         System.out.println("advanced search");
     }
 
