19991213cb21d13383543dda60a623a7cd3a0ad2
Hughbert001 <longhr@vcu.edu>
2018-04-20 15:59:14 -0400

    Added Controller_AdvancedSearch Java

---
+++ src/festival_package/Controller_AdvancedSearch.java
@@ -0,0 +1,13 @@
+package festival_package;
+
+import javafx.event.ActionEvent;
+
+
+
+public class Controller_AdvancedSearch {
+
+    public void on_search_button(ActionEvent event)
+    {
+        System.out.println("Fuuuuck!");
+    }
+}
